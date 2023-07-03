d41fee6ed625ebbb19c6f165feeb52d47558e3a3
Marco <pimientapfoehler@gmail.com>
2023-07-03 16:15:16 +0200

    neu

---
+++ src/Mail.java
@@ -0,0 +1,46 @@
+public class Mail {
+
+    //Att
+    private String sender;
+    private String recipient;
+    private String subject;
+    private String message;
+
+
+    public Mail() {
+
+    }
+
+    public String getSender() {
+        return this.sender;
+    }
+    //bei boolean is/has
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
